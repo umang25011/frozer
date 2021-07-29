76a43f81ab28161eeb6cc08adcf31413e4d885d4
Umang Patel <Umang25011@gmail.com>
2021-07-29 21:53:43 +0530

    Working inject.js code

--- public/app/background.js
+++ public/app/background.js
@@ -1,20 +1,6 @@
-const URLS = ["<all_urls>"]
-
-chrome.webRequest.onBeforeSendHeaders.addListener(
-  function (details) {
-    console.log("onBeforeSendHeaders", details)
-  },
-  { urls: URLS },
-  ["blocking", "requestHeaders", "extraHeaders"]
-)
-
-chrome.webRequest.onBeforeRequest.addListener(
-  function (details) {
-    const postedString = decodeURIComponent(
-      String.fromCharCode.apply(null, new Uint8Array(details.requestBody.raw[0].bytes))
-    )
-    console.log("onBeforeRequest", postedString)
-  },
-  { urls: URLS },
-  ["blocking", "requestBody"]
-)
+var s = document.createElement("script")
+s.src = chrome.runtime.getURL("app/inject.js")
+s.onload = function () {
+  this.remove()
+}
+;(document.head || document.documentElement).appendChild(s)
